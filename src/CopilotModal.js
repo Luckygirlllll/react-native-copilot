51487830673ea457ad88f21ae11e8d4c034055f2
Iryna <irynakanivets@gmail.com>
2018-03-19 16:35:05 +0100

    Chnaged arrow design

--- src/CopilotModal.js
+++ src/CopilotModal.js
@@ -106,11 +106,17 @@ class CopilotModal extends Component<Props, State> {
       tooltip.right = tooltip.right === 0 ? tooltip.right + MARGIN : tooltip.right;
       tooltip.maxWidth = layout.width - tooltip.right - MARGIN;
       arrow.right = tooltip.right + MARGIN;
+      arrow.borderBottomWidth=30;
+      arrow.borderLeftWidth= 30;
+      arrow.borderRightWidth= 30;
     } else {
       tooltip.left = Math.max(obj.left, 0);
       tooltip.left = tooltip.left === 0 ? tooltip.left + MARGIN : tooltip.left;
       tooltip.maxWidth = layout.width - tooltip.left - MARGIN;
       arrow.left = tooltip.left + MARGIN;
+      arrow.borderTopWidth=30;
+      arrow.borderLeftWidth= 30;
+      arrow.borderRightWidth= 30;
     }
 
     const animate = {
